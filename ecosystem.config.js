c47a33424eda20bcaefad8c9594d34170601a731
Pascal <p.parent.1@outlook.com>
2025-02-07 10:47:21 -0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'wordament-server',
-      script: './dist/server.js',
+      script: './dist/src/server.js',
       instances: 1,
       autorestart: true,
       watch: false,
